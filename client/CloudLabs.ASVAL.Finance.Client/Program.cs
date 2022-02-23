b6144f3618b24693ab9ff7f50006f05d5e2811bf
krzysztofla <krzysztof.lach@icloud.com>
2022-02-23 21:54:55 +0100

    Another try to configure web grpc - updated p1

--- client/CloudLabs.ASVAL.Finance.Client/Program.cs
+++ client/CloudLabs.ASVAL.Finance.Client/Program.cs
@@ -21,4 +21,3 @@ builder.Services.AddSingleton(services =>
 builder.Services.AddScoped(sp => new HttpClient {BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)});
 
 await builder.Build().RunAsync();
-
--- src/CloudLabs.ASVAL.Finance.Currency.Feed/Program.cs
+++ src/CloudLabs.ASVAL.Finance.Currency.Feed/Program.cs
@@ -1,3 +1,4 @@
+
 using CloudLabs.ASVAL.Finance.Currency.Feed.Services;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,13 +8,22 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddGrpc();
+builder.Services.AddCors(o => o.AddPolicy("AllowAll", builder =>
+{
+    builder.AllowAnyOrigin()
+        .AllowAnyMethod()
+        .AllowAnyHeader()
+        .WithExposedHeaders("Grpc-Status", "Grpc-Message", "Grpc-Encoding", "Grpc-Accept-Encoding");
+}));
 
 var app = builder.Build();
+app.UseRouting();
 
-// Configure the HTTP request pipeline.
-app.MapGrpcService<GreeterService>();
-app.MapGet("/",
-    () =>
-        "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+app.UseGrpcWeb(); 
+app.UseCors();
 
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapGrpcService<GreeterService>().EnableGrpcWeb().RequireCors("AllowAll");
+});
 app.Run();
